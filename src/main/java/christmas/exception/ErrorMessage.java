b37dfc3870085f12a3e23da9aa4bafb0707ff1d4
takealook97 <takealook97@naver.com>
2023-11-15 04:02:12 +0900

    style: 상수명 변경 및 불필요한 상수 삭제
    실수로 기재했던 상수를 삭제하였다. 추가적으로, Weekday관련 네이밍을 수정했으며 에러 메세지에 입력 에러 관련 메세지를 추가했다.

--- src/main/java/christmas/exception/ErrorMessage.java
+++ src/main/java/christmas/exception/ErrorMessage.java
@@ -1,6 +1,7 @@
 package christmas.exception;
 
 public enum ErrorMessage {
+	EMPTY_ERROR("[ERROR] 입력을 하지 않았습니다. 다시 입력해 주세요."),
 	DAY_ERROR("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
 	ORDER_ERROR("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
 	SUM_PRICE_ERROR("[ERROR] 총주문 금액 10,000원 이상부터 이벤트가 적용됩니다. 다시 입력해 주세요."),
