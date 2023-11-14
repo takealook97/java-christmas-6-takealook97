9a3fa55f5c3a08c948c062ec1d014496f42902c8
takealook <takealook97@naver.com>
2023-11-14 19:13:10 +0900

    feat: BenefitConstant Enum 생성 및 구현
    혜택과 관련된 상수를 enum 처리 하였다.

---
+++ src/main/java/christmas/domain/constant/BenefitConstant.java
@@ -0,0 +1,22 @@
+package christmas.domain.constant;
+
+import static christmas.domain.constant.EventTime.*;
+
+public enum BenefitConstant {
+	D_DAY(25),
+	DISCOUNT_INIT_PRICE(1000),
+	DAILY_DISCOUNT_INCREMENT(100),
+	STAR_DISCOUNT(1000),
+	GIFT_QUALIFYING_PRICE(120000),
+	YEAR_BASED_DISCOUNT(EVENT_YEAR.getValue());
+	
+	private final long value;
+
+	BenefitConstant(long value) {
+		this.value = value;
+	}
+
+	public long getValue() {
+		return value;
+	}
+}
